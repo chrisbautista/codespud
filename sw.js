d9e2689878e86078dfa742a181bf9f54edad798d
chrisbautista <christopher.bautista@gmail.com>
2020-06-27 22:30:29 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-230c8c3c2574bacac877.js"
+    "url": "webpack-runtime-3516ec701728f8efc088.js"
   },
   {
     "url": "framework-8dcecaaefd71e2213eb2.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "27264038406e0cd916717c300d657d58"
+    "revision": "93b2d39fda30565fda0bba87fc5a65cd"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6d1e54e7b4e22a5a9d12de205999ec40"
+    "revision": "f1270a2b60286e90fabcf257b968e1dd"
   },
   {
     "url": "manifest.webmanifest",
