94a905311ed86a5e6aaadc45f75ddf25c79a1ae0
chrisbautista <chris@codespud.com>
2022-12-29 16:32:38 -0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-82fc8f77ed53daa06050.js"
+    "url": "webpack-runtime-3f19645d6fa4cf5129fb.js"
   },
   {
     "url": "framework-bd6835f73ebf17dd51d8.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3d6543d6f3b42e631a586a04a03a4249"
+    "revision": "a7ec8d5a0e060e5652ba58517b9dbbce"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0d13ec9ed6552393d00c757ab4fd53ad"
+    "revision": "1c5717be65ad147ff86afc33a2d0a523"
   },
   {
     "url": "polyfill-cab41bb2a8f9008f4da1.js"
